c6cb1d388b2f86a496221e259cde7e578112ef43
henrique221 <hborges9294@gmail.com>
2019-05-08 02:25:23 -0300

    improving list mobile

---
+++ public/js/animate.js
@@ -0,0 +1,15 @@
+function animateCSS(elementId, animationName, callback) {
+
+    var node = document.getElementById(elementId);
+
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+        node.classList.remove('animated', animationName)
+        node.removeEventListener('animationend', handleAnimationEnd)
+
+        if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+}
